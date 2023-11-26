bf4d47d0e3f5b7e246627d7c9d0a98772ed95289
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2023-11-26 22:42:31 +0800

    Change timer to count down

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -6,35 +6,37 @@ using TMPro;
 public class GameTimer : MonoBehaviour
 {
     public float seconds = 0.0f;
-    public int minutes = 0;
-    public int hours = 0;
+    public int minutes = 2;
+    //public int hours = 0;
 
     TextMeshProUGUI text;
 
     // Start is called before the first frame update
     void Start()
     {
-        seconds = 0.0f;
-        minutes = 0;
-        hours = 0;
+        //seconds = 0.0f;
+        //minutes = 0;
+        //hours = 0;
         text = GetComponent<TextMeshProUGUI>();
-        text.text = "Time " + hours + " : " + minutes + " : " + seconds.ToString("F1");
+        text.text = "Time " + minutes + " : " + seconds.ToString("F1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        seconds += Time.deltaTime;
-        if (seconds >= 60.0f)
+        seconds -= Time.deltaTime;
+        if (seconds <= 0.0f)
         {
-            seconds -= 60.0f;
-            minutes += 1;
+            seconds += 60.0f;
+            minutes -= 1;
         }
+        /*
         if (minutes >= 60)
         {
             minutes -= 60;
             hours += 1;
         }
-        text.text = "Time " + hours + " : " + minutes + " : " + seconds.ToString("F1");
+        */
+        text.text = "Time " + minutes + " : " + seconds.ToString("F1");
     }
 }
