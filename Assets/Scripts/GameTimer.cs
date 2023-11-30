5d5eec13656fbb5d4ec2d0c27a08e7786e79e3d2
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2023-11-30 13:21:39 +0800

    Add GameTimer stop and restart

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -10,6 +10,12 @@ public class GameTimer : MonoBehaviour
     public float seconds = 0.0f;
     public int minutes = 2;
     //public int hours = 0;
+    [SerializeField]
+    private bool isGameTimerRunning = true;
+    [SerializeField]
+    private float initialSeconds = 0.0f;
+    [SerializeField]
+    private int initialMinutes = 2;
 
     TextMeshProUGUI text;
 
@@ -28,6 +34,8 @@ public class GameTimer : MonoBehaviour
         //seconds = 0.0f;
         //minutes = 0;
         //hours = 0;
+        seconds = initialSeconds;
+        minutes = initialMinutes;
         text = GetComponent<TextMeshProUGUI>();
         text.text = ""+minutes + ":" + seconds.ToString("00"); //seconds.ToString("F1") for 1 decimal
     }
@@ -36,18 +44,33 @@ public class GameTimer : MonoBehaviour
     void Update()
     {
         seconds -= Time.deltaTime;
-        if (seconds <= 0.0f)
+        if (isGameTimerRunning == true)
         {
-            seconds += 60.0f;
-            minutes -= 1;
+            if (seconds <= 0.0f)
+            {
+                seconds += 60.0f;
+                minutes -= 1;
+            }
+            if (seconds > 59.0f && seconds <= 60.0f)
+            {
+                text.text = "" + (minutes + 1) + ":" + "00"; //when seconds is 60, display as 0 and minutes+1
+            }
+            else if (seconds <= 59.0f)
+            {
+                text.text = "" + minutes + ":" + seconds.ToString("00");
+            }
         }
-        if (seconds > 59.0f && seconds <= 60.0f)
-        {
-            text.text = "" + (minutes+1) + ":" + "00"; //when seconds is 60, display as 0 and minutes+1
-        }
-        else if (seconds <= 59.0f)
-        {
-            text.text = "" + minutes + ":" + seconds.ToString("00");
-        }
+    }
+
+    public void StopGameTimer()
+    {
+        isGameTimerRunning = false;
+    }
+
+    public void RestartGameTimer()
+    {
+        seconds = initialSeconds;
+        minutes = initialMinutes;
+        isGameTimerRunning = true;
     }
 }
