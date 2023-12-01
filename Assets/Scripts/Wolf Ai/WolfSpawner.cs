c7d053760d9ae8a3704436ab76ac66a8062227f8
Uon <49972061+Orelios@users.noreply.github.com>
2023-12-01 23:17:12 +0800

    Refactor wolf spawner

--- Assets/Scripts/Wolf Ai/WolfSpawner.cs
+++ Assets/Scripts/Wolf Ai/WolfSpawner.cs
@@ -1,35 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class WolfSpawner : MonoBehaviour
+public class WolfSpawner : MonoBehaviourPunCallbacks
 {
     public GameObject wolf;
-    public float maxSpawnTimer;
+    [SerializeField]
+    private string wolfId; //What is the id of the prefab we want to spawn from the pool
+    [SerializeField]
+    private float maxSpawnTimer;
     private float spawnTimer;
+    private bool isSpawning;
+    [SerializeField]
+    private Vector3 spawnPosition;
+
     void Start()
     {
-        SetTimer(); 
+        //SetTimer();
     }
 
     void Update()
     {
-        if(spawnTimer <= 0)
+        //Don't do anything if you are not the master client
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        if (!isSpawning)
+            StartCoroutine(SpawnCoroutine());
+
+        /*
+        if (spawnTimer <= 0)
         {
             SpawnWolves();
             SetTimer();
         }
         else
         {
-            spawnTimer -= Time.deltaTime; 
+            spawnTimer -= Time.deltaTime;
+        }*/
+    }
+
+    IEnumerator SpawnCoroutine()
+    {
+        //Generate a random waiting interval
+        //spawnInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
+        isSpawning = true;
+        //Wait for the spawnInterval
+        yield return new WaitForSeconds(maxSpawnTimer);
+        //SpawnEnemy();
+        SpawnOverNetwork();
+        isSpawning = false;
+    }
+
+    private void SpawnOverNetwork()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.InstantiateRoomObject(wolfId, GetSpawnPosition(), Quaternion.identity);
         }
     }
-    private void SetTimer()
+    private void SpawnWolf()
     {
-        spawnTimer = maxSpawnTimer; 
+        //Get an object from the pool
+        GameObject enemy = ObjectPoolManager.Instance.GetPooledObject(wolfId);
+        //Did we get an object from the pool?
+        if (enemy != null)
+        {
+            //Position the enemy
+            enemy.transform.position = GetSpawnPosition();
+            enemy.SetActive(true);
+        }
     }
-    private void SpawnWolves()
+
+    private Vector3 GetSpawnPosition()
     {
-        ObjectPoolManager.SpawnObject(wolf, transform.position, transform.rotation, ObjectPoolManager.PoolType.Enemies);
+        return spawnPosition;
     }
+    // FORMER FUNCTIONS
+    //private void SetTimer()
+    //{
+    //    spawnTimer = maxSpawnTimer;
+    //}
+    //private void SpawnWolves()
+    //{
+    //    ObjectPoolManager.SpawnObject(wolf, transform.position, transform.rotation, ObjectPoolManager.PoolType.Enemies);
+    //}
 }
