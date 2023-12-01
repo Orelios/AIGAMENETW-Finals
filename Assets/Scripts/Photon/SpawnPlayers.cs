e01a607db27c74c2f254894c562fca56f1f262a2
Uon <49972061+Orelios@users.noreply.github.com>
2023-12-01 16:13:08 +0800

    Add photon view

--- Assets/Scripts/Photon/SpawnPlayers.cs
+++ Assets/Scripts/Photon/SpawnPlayers.cs
@@ -7,12 +7,14 @@ public class SpawnPlayers : MonoBehaviour
 {
     //public GameObject playerPrefab;
     //public Vector3 p1SpawnPos;
-
+    //public static SpawnPlayers instance;
+    //public Transform[] spawnPoints;
     public GameObject playerPrefab;
     public Vector3 spawnPos;
 
     private void Start()
     {
+        //instance = this;
         spawnPos = new Vector3(0f, 3f, 0f); // Player 1 spawn position
         PhotonNetwork.Instantiate(playerPrefab.name, spawnPos, Quaternion.identity);
     }
