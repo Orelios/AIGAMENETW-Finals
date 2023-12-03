bfb9a54c66d3fae7e9eff3457699518714d7929b
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2023-12-03 22:11:33 +0800

    Add HitCooldown.cs

---
+++ Assets/Scripts/Shooting/HitCooldown.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCooldown : MonoBehaviour
+{
+    public bool isTimerRunning = false;
+    public float hitCooldown = 1.0f;
+    public float countdown = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTimerRunning == true)
+        {
+            if (countdown > 0)
+            {
+                countdown -= Time.deltaTime;
+            }
+            else
+            {
+                isTimerRunning = false;
+                countdown = hitCooldown;
+                //Teleport();
+                //GetComponent<Character>().Activate();
+                //GetComponent<Character>().ResetHealth();
+                //GetComponent<FallDown>().StopFalling();
+            }
+        }
+    }
+
+    public void StartHitCooldown()
+    {
+        isTimerRunning = true;
+    }
+}
