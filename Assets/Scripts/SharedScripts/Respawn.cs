1c1d7ba2661428d6b0a4b16a1267b35bf99ad871
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2023-12-03 22:39:59 +0800

    Change Respawn coordinates to 0,5,0

--- Assets/Scripts/SharedScripts/Respawn.cs
+++ Assets/Scripts/SharedScripts/Respawn.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
-    public float spawnX, spawnY, spawnZ;
+    //public float spawnX = 0, spawnY = 5, spawnZ = 0;
     public bool isTimerRunning = false;
     public float respawnDelay = 5.0f;
     public float countdown = 5.0f;
@@ -39,7 +39,7 @@ public class Respawn : MonoBehaviour
     public void Teleport()
     {
         GetComponent<CharacterController>().enabled = false;
-        GetComponent<CharacterController>().transform.position = new Vector3(spawnX, spawnY, spawnZ);
+        GetComponent<CharacterController>().transform.position = new Vector3(0, 5, 0);
         GetComponent<CharacterController>().enabled = true;
     }
 
